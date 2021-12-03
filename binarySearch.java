e0a7d6dfe60de0250bc16b40c2f602dc2f89bfeb
Ayush das <ayush24das@gmail.com>
2021-12-03 22:33:32 +0530

    Binary search

---
+++ binarySearch.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class binarySearch {
+    public static int bs(int[] arr, int x){
+        int li = 0, ri = arr.length - 1;
+
+        while(li<=ri){
+            int mid = (li + ri)/2;
+            if(arr[mid] == x){
+                return mid;
+            }else if(arr[mid] > x){
+                // left me dhundho
+                ri = mid-1;
+            }else{
+                li = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] arr = {4,6,8,10,12,14,16,18,20,22,24};
+        // int x = 20;
+        // int x = 24;
+        int x = 11;
+
+        int ans = bs(arr, x);
+        System.out.println(ans);
+    }
+}

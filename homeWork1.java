1da270ab9e390aa2be284d675ce0c33e565165ca
Ayush das <ayush24das@gmail.com>
2021-12-07 20:17:42 +0530

    hw1.1 done

--- homeWork1.java
+++ homeWork1.java
@@ -12,10 +12,15 @@ public class homeWork1 {
         }
 
         for(int j = 0; j<m ;j++){
-            for(int i = 0; i<n;i++){
-                System.out.print(a[i][j]);
+            if(j%2==0){
+                for(int i = n-1; i>=0;i--){
+                    System.out.print(a[i][j] + " ");
+                }
+            }else{
+                for(int i = 0; i<n;i++){
+                    System.out.print(a[i][j] + " ");
+                }
             }
-            System.out.println();
         }
     }
 }

7c35890f9d97a65390aca133fdca453f5aba1176
Ayush das <ayush24das@gmail.com>
2021-12-13 15:55:59 +0530

    pattern20 added

--- Revision/Patterns/pattern20.java
+++ Revision/Patterns/pattern20.java
@@ -1,3 +1,8 @@
+// *                               *
+// *                               *
+// *               *               *
+// *       *               *       *
+// *                               *
 import java.util.Scanner;
 public class pattern20{
     public static void pattern20(int n){

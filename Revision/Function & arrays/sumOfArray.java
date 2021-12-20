08e41d570a9ff546a3b903f085614e1956470b6c
Ayush das <ayush24das@gmail.com>
2021-12-20 20:20:27 +0530

    sum of arrays done

--- Revision/Function & arrays/sumOfArray.java
+++ Revision/Function & arrays/sumOfArray.java
@@ -1,62 +1,65 @@
 import java.util.Scanner;
 public class sumOfArray {
-    public static void sumOfArray(int[] A, int[] B){
+    public static int[] sumOfArray(int[] A, int[] B){
         int n1 = A.length;
         int n2 = B.length;
-
         int[] ans = new int[(n1>n2)?n1:n2];
 
         int i = n1-1;
         int j = n2-1;
-        int k = ans.length - 1;
+        int k = ans.length-1;
+
         int carry = 0;
+
         while(k>=0){
-            int sum = carry ;
+            int sum = 0;
+            sum += carry;
+
             if(i>=0){
                 sum += A[i];
             }
-
             if(j>=0){
-                sum += B[j]; 
+                sum+= B[j];
             }
 
+            int val = sum%10;
             carry = sum/10;
-            sum%=10;
 
-            ans[k] = sum;
+            ans[k] = val;
 
             i--;
             j--;
             k--;
+
         }
 
         if(carry != 0){
             System.out.println(carry);
         }
 
-        for( int val: ans){
-            System.out.println(val);
-        }
-
+        return ans;
     }
 
+    public static void display(int[] ans){
+        for(int val : ans){
+            System.out.println(val);
+        }
+    }
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         int n1 = scn.nextInt();
-        int[] arr1 = new int[n1];
-
-        for(int i = 0; i<arr1.length; i++){
-            arr1[i] = scn.nextInt();
+        int[] A = new int[n1];
+        for(int i = 0; i<A.length; i++){
+            A[i] = scn.nextInt();
         }
 
         int n2 = scn.nextInt();
-        int[] arr2 = new int[n2];
-
-        for(int i = 0; i<arr2.length; i++){
-            arr2[i] = scn.nextInt();
+        int[] B = new int[n2];
+        for(int i = 0; i<B.length ;i++){
+            B[i] = scn.nextInt();
         }
 
-        System.out.println("'ANSWER'");
-        sumOfArray(arr1, arr2);
+        int[] ans = sumOfArray(A, B);
+        display(ans);
     }
 }

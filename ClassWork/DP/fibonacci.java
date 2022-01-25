59480a7af333ebacfdfd5619189ebc533fe0691f
Ayush das <ayush24das@gmail.com>
2022-01-25 08:32:38 +0530

    fibonacci by DP done

---
+++ ClassWork/DP/fibonacci.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void display(int[] dp){
+        for(int e : dp){
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    public static int fiboR(int n, int[] dp){
+        if(n<=1){
+            return dp[n] = n;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int nm1 = fiboR(n-1, dp);
+        int nm2 = fiboR(n-2, dp);
+
+        return dp[n] = nm1 + nm2;
+    }
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        int ans = fiboR(n, dp);
+        display(dp);
+
+        System.out.println(ans);
+
+    }
+}

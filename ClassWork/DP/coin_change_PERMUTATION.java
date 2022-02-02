133deee88e9156ada13bb46422778825f04867c0
iamayushdas <ayush24das@gmail.com>
2022-02-02 13:36:14 +0530

    coin change permutation done

---
+++ ClassWork/DP/coin_change_PERMUTATION.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class coin_change_PERMUTATION {
+    public static int coinChangePermutation(int[] arr, int amt){
+        int [] dp = new int[amt + 1];
+        dp[0] = 1;
+        for(int j = 0; j<dp.length; j++){
+            for(int i = 0; i<arr.length; i++ ){
+                if(j-arr[i]>=0){
+                    dp[j] += dp[j- arr[i]];
+                }
+            }
+        }
+        return dp[amt];
+    }
+    public static void main(String[] args){
+        Scanner  scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        int amt = scn.nextInt();
+        int ans = coinChangePermutation(arr, amt);
+        System.out.println(ans);
+    }
+}

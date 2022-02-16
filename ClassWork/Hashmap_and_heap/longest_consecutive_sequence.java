ac09744c221163851ca5b85944b190d95f3772d3
iamayushdas <ayush24das@gmail.com>
2022-02-16 22:00:03 +0530

    longest consecutive sequence

---
+++ ClassWork/Hashmap_and_heap/longest_consecutive_sequence.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+import javax.swing.text.MaskFormatter;
+
+import java.util.HashMap;
+
+public class longest_consecutive_sequence {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scn.nextInt();
+        }
+
+        HashMap<Integer, Boolean> hm = new HashMap<>();
+        for (int ele : a) {
+            hm.put(ele, true);
+        }
+
+        for (int ele : a) {
+            if (hm.containsKey(ele - 1)) {
+                hm.put(ele, false);
+            }
+        }
+
+        int maxLen = 0;
+        int maxStPt = 0;
+
+        for (int ele : a) {
+            if (hm.get(ele) == true) {
+                int tempLen = 1;
+                int tempStPt = ele;
+
+                while (hm.containsKey(tempLen + tempStPt)) {
+                    tempLen++;
+                }
+
+                if (tempLen > maxLen) {
+                    maxLen = tempLen;
+                    maxStPt = tempStPt;
+                }
+            }
+        }
+
+        for (int i = maxStPt; i < maxStPt + maxLen; i++) {
+            System.out.println(i);
+        }
+    }
+}

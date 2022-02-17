ac967313bbc00b9c9ef0dd5eecb55fe0c95434e5
iamayushdas <ayush24das@gmail.com>
2022-02-17 08:05:18 +0530

    max priority queue op done

--- ClassWork/Hashmap_and_heap/PriorityQueues.java
+++ ClassWork/Hashmap_and_heap/PriorityQueues.java
@@ -1,18 +1,37 @@
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class PriorityQueues {
-    public static void main(String[] args){
-        int[] arr = {1,21,57,92,13,43,22,21,6};
+    public static void main(String[] args) {
 
-        // create 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();   //by default min PQ
-        for(int ele : arr){
-            pq.add(ele);      // T.C -> for 1 ele = log(n) so, for adding n elements nlog(n)
+        int[] arr = { 1, 21, 57, 92, 13, 43, 22, 21, 6 };
+
+        // create -MIN priority queue
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(); //by default min PQ
+        // for(int ele : arr){
+        // pq.add(ele); // T.C -> for 1 ele = log(n) so, for adding n elements nlog(n)
+
+        // }
+        // while(pq.size() != 0){
+        // System.out.println(pq.peek());
+        // pq.remove(); // T.C -> for 1 ele = log(n) so, for adding n elements nlog(n)
+        // }
+
+        // total T.C is 2nlog(n) ~ nlog(n)
+
+
+
+
+        // create - MAX priority queue
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // for Max priority queue
+        for (int ele : arr) {
+            pq.add(ele); // T.C -> for 1 ele = log(n) so, for adding n elements nlog(n)
 
         }
-        while(pq.size() != 0){
+        while (pq.size() != 0) {
             System.out.println(pq.peek());
-            pq.remove();
+            pq.remove(); // T.C -> for 1 ele = log(n) so, for adding n elements nlog(n)
         }
+
     }
 }

f4ce71a728c5c9ad00743c06bdf49e79cedbfcdd
iamayushdas <ayush24das@gmail.com>
2022-03-10 22:14:21 +0530

    stack demo

---
+++ ClassWork/Stack_and_queue/stackDemo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class stackDemo {
+    public static void main(String[] args){
+        //stack creation
+        Stack<Integer> st = new Stack<>();
+
+                     // stack overflow
+        // st.push(10);
+        // st.push(20);
+        // st.push(30); 
+        // st.push(40);
+        // st.push(50);
+
+        // while(true){
+        //     st.push(10);
+        // }
+
+                    // stack underflow
+        st.peek();
+    }
+}

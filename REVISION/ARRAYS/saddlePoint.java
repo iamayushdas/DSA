516d1456f4c38b6a3455d5ce99ccb79b0cef31d2
iamayushdas <ayush24das@gmail.com>
2022-07-18 20:57:22 +0530

    saddle point done and dusted

--- REVISION/ARRAYS/saddlePoint.java
+++ REVISION/ARRAYS/saddlePoint.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class saddlePoint {
     public static void main(String[] args){
-        Scanner scn = new Scanner(System.in)
+        Scanner scn = new Scanner(System.in);
             int n = scn.nextInt();
 
             int[][] arr = new int[n][n];
@@ -12,7 +12,35 @@ public class saddlePoint {
                     arr[i][j] = scn.nextInt();
                 }
             }
+
+        saddlePoint(arr);
     }
 
-    
+    public static void saddlePoint(int[][] arr) {
+        for(int i = 0; i<arr.length; i++){
+
+            // column ka chota element nikaalne k liye
+            int lcvi = 0;
+            for(int j = 1; j<arr[0].length; j++){
+                if(arr[i][lcvi] > arr[i][j]){
+                    lcvi = j;
+                }
+            }
+
+            boolean flag = true;
+            for(int k = 0; k<arr.length; k++){
+                if(arr[k][lcvi] > arr[i][lcvi]){
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag == true){
+                System.out.println(arr[i][lcvi]);
+                return;
+            }
+        }
+
+        System.out.println("invalid input");
+    }
 }

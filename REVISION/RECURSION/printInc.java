07b7f67af6d9e66b488fe83d4de8d98ca88426e1
iamayushdas <ayush24das@gmail.com>
2022-07-22 21:46:57 +0530

    print  increasing rec

---
+++ REVISION/RECURSION/printInc.java
@@ -0,0 +1,21 @@
+package REVISION.RECURSION;
+
+import java.util.Scanner;
+
+public class printInc {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n==0){
+            return;
+        }
+        printIncreasing(n-1);;
+        System.out.println(n);
+    }
+}
+
+

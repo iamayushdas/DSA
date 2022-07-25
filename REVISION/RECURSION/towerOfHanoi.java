81011449cd8949eaca27316043c38346cf0e43aa
iamayushdas <ayush24das@gmail.com>
2022-07-25 23:24:18 +0530

    tower of hanoi done

---
+++ REVISION/RECURSION/towerOfHanoi.java
@@ -0,0 +1,24 @@
+package REVISION.RECURSION;
+
+import java.util.Scanner;
+
+public class towerOfHanoi {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1d = scn.nextInt();
+        int t2d = scn.nextInt();
+        int t3d = scn.nextInt();
+        toh(n, t1d, t2d, t3d);
+    }
+
+    public static void toh(int n, int t1id, int t2id, int t3id){
+        if(n==0) return;
+        toh(n-1, t1id, t3id, t2id);
+        System.out.println(n+"[" + t1id + " -> " + t2id + "]");
+        toh(n-1, t3id, t2id, t1id);
+    }
+
+}
+
+
